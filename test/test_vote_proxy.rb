9be137816c30b20b80f5cf431f051cca1ab84e7e
Kenneth Bruce Uy <missingno15@gmail.com>
2015-05-29 15:27:21 -0400

    Stub out VoteProxy tests

--- test/test_vote_proxy.rb
+++ test/test_vote_proxy.rb
@@ -4,14 +4,14 @@ require "sousenkyo/vote_proxy"
 class TestVoteProxy < Minitest::Test
   attr_reader :valid_voting_proxy,
               :invalid_voting_proxy
-  def setup
-    serial_codes = [["dd289zfe", "f2euqv6b"]]
-    @valid_vote_proxy = Sousenkyo::VoteProxy.new(serial_codes, "")
-  end
+  # def setup
+  #   serial_codes = [["dd289zfe", "f2euqv6b"]]
+  #   @valid_vote_proxy = Sousenkyo::VoteProxy.new(serial_codes, "")
+  # end
   
-  def teardown
-    @valid_vote_proxy.send(:browser).quit
-  end
+  # def teardown
+  #   @valid_vote_proxy.send(:browser).quit
+  # end
 
   def test_it_raises_year_mismatch_code_error
     skip "to be implemented"
